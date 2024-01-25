771471f44b677ab23fea0b213d7e2e48845a8260
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-25 15:01:06 +0100

    2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -35,3 +35,16 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
         return self.__dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get hypermedia pagination"""
+        data = self.get_page(page, page_size)
+        total_pages = len(self.__dataset) // page_size
+        if len(self.__dataset) % page_size != 0:
+            total_pages += 1
+        return {'page_size': len(data),
+                'page': page,
+                'data': data,
+                'next_page': page + 1 if page < total_pages else None,
+                'prev_page': page - 1 if page > 1 else None,
+                'total_pages': total_pages}
