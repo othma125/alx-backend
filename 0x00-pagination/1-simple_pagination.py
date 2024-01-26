f714beaa1c533279268fae26b7573a3e0e34c09e
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-26 10:10:59 +0100

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -36,6 +36,7 @@ class Server:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
-        if start > len(__dataset()):
+        dataset = self.dataset()
+        if start > len(dataset):
             return []
-        return self.__dataset()[start:end]
+        return dataset[start:end]
