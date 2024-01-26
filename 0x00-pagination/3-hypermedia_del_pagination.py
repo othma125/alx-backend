738960c50cc2b7bf5e73e0fb1b64cfff0262ed19
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-26 10:17:10 +0100

    3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+"""Deletion-resilient hypermedia pagination
 """
-Deletion-resilient hypermedia pagination
-"""
-
 import csv
-from typing import List
+from typing import Dict, List
 
 
 class Server:
@@ -13,6 +11,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -39,19 +39,26 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Get hypermedia pagination"""
-        assert isinstance(index, int) and index >= 0
-        assert isinstance(page_size, int) and page_size > 0
-        indexed_dataset = self.indexed_dataset()
-        data = []
-        next_index = index + page_size
-        while index < next_index:
-            if indexed_dataset.get(index):
-                data.append(indexed_dataset[index])
-            else:
-                next_index += 1
-            index += 1
-        return {'index': indexed_dataset[index],
-                'data': data,
-                'page_size': len(data),
-                'next_index': next_index}
+        """Retrieves info about a page from a given index and with a
+        specified size.
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        page_data = []
+        count = 0
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and count < page_size:
+                page_data.append(item)
+                count += 1
+                continue
+            if count == page_size:
+                next_index = i
+                break
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(page_data),
+            'data': page_data,
+        }
